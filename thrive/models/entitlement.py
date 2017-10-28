cf0600c8e3d0d70289a02f863bc7b266aa9ffa73
Pedro Guzman <pguzmanb498@ulacit.ed.cr>
2017-10-28 00:35:45 +0000

    Cha nges

--- thrive/models/entitlement.py
+++ thrive/models/entitlement.py
@@ -1,13 +1,31 @@
+# ------------------------------------------------------------------------------
+# CLASS ENTITLEMENT
+# ------------------------------------------------------------------------------
 class Entitlement:
     
+    """
+        An entitlement establishes a relationship between an organization and a 
+        user. The relationships represents an action that a given user is entitled
+        to do within the context of a given organization. 
+    """
+    
+    # --------------------------------------------------------------------------
+    # CONSTRUCTOR
+    # --------------------------------------------------------------------------
     def __init__(self, org_id, user_id, **kwargs):
         self.org_id = org_id
         self.user_id = user_id
         if 'entitlement_id' in kwargs:
             self.entitlement_id = kwargs['entitlement_id']
     
-    def state_valid(self):
+    # --------------------------------------------------------------------------
+    # METHOD IS STATE VALID
+    # --------------------------------------------------------------------------
+    def is_state_valid(self):
         pass
     
+    # --------------------------------------------------------------------------
+    # METHOD SAVE
+    # --------------------------------------------------------------------------
     def save(self):
         pass
